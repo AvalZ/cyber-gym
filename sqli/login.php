b1b904826e075353623b1834899a016a40479987
avalz <avalenza89@gmail.com>
2016-05-23 21:59:29 +0200

    First commit

---
+++ sqli/login.php
@@ -0,0 +1,23 @@
+<?php
+include('mysql_credentials.php');
+
+$con = new mysqli( $mysql_server, $mysql_user, $mysql_pass, $mysql_db );
+
+if ($con->connect_error) die ("Connection failed: " .$con->connect_error);
+
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+$query = "SELECT * FROM accounts WHERE email='$email' AND password=SHA('$pass')";
+$result = $con->query($query);
+
+if($result->num_rows > 0) {
+  $row = $result->fetch_assoc();
+  $first_name = $row["first_name"];
+  $last_name = $row["last_name"];
+  echo "Login successful. Welcome $first_name $last_name!";
+} else {
+  echo "Wrong username or password";
+}
+
+$con->close();
---
+++ sqli/mysql_credentials.php
@@ -0,0 +1,7 @@
+<?php
+
+$mysql_server = "localhost";
+$mysql_user = getenv('MYSQL_USER');
+$mysql_pass = getenv('MYSQL_PASS');
+$mysql_db = "websec"; 
+
