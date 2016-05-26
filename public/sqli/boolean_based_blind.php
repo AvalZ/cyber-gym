8fecdb9a470976a083519344a9e0b9f24bc5fcfb
avalz <avalenza89@gmail.com>
2016-05-26 12:24:26 +0200

    Add Boolean-Based Blind injection

---
+++ public/sqli/boolean_based_blind.php
@@ -0,0 +1,29 @@
+<?php
+include('mysql_credentials.php');
+
+if (!isset($_GET['email'])) {
+  echo "<form action=''>";
+  echo "<label for='email'>Validate email: does this email exist in the DB?</label><input name='email'>";
+  echo "</form>";
+} else {
+  $email = $_GET['email'];
+  
+  $con = new mysqli( $mysql_server, $mysql_user, $mysql_pass, $mysql_db );
+  $query = "SELECT * FROM accounts WHERE email='$email'";
+  //    SELECT * FROM accounts WHERE email='' UNION SELECT 1,User(),(SELECT user FROM mysql.user LIMIT 0,1),4,'5 ';
+
+  $result = $con->query($query);
+
+  if ($con->error) {
+    echo "Query error: " .$con->error;
+  }
+
+
+  if( $row = $result->num_rows > 0) {
+    echo "YES";
+  } else {
+    echo "NO";
+  }
+
+
+}
