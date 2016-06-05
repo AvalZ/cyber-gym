4d7bf2fe667e6f746c3ac6b7077924a5ef220788
avalz <avalenza89@gmail.com>
2016-06-05 17:15:51 +0200

    Add Stacked Queries to SQLi attacks.

---
+++ public/sqli/stacked_queries.php
@@ -0,0 +1,38 @@
+<?php
+include('../db/mysql_credentials.php');
+
+if (!isset($_GET['email'])) {
+  echo "<form action=''>";
+  echo "<label for='email'>Search by email</label><input name='email'>";
+  echo "</form>";
+} else {
+  $email = $_GET['email'];
+  
+  $con = new mysqli( $mysql_server, $mysql_user, $mysql_pass, $mysql_db );
+  $query = "SELECT * FROM accounts WHERE email='$email'";
+  //    SELECT * FROM accounts WHERE email='' UNION SELECT 1,User(),(SELECT user FROM mysql.user LIMIT 0,1),4,'5 ';
+
+  var_dump($query);
+
+  $con->multi_query($query);
+
+  do {
+    if ($result = $con->store_result()) {
+
+      echo "<table>";
+      echo "<tr><th>First Name</th><th>Last Name</th><th>Email</th></tr>";
+
+      while( $row = $result->fetch_assoc() ) {
+        $first_name = $row['first_name'];
+        $last_name = $row['last_name'];
+        $email = $row['email'];
+
+        echo "<tr><td>$first_name</td><td>$last_name</td><td>$email</td></tr>";
+      }
+      echo "</table>";
+    }
+  } while (@$con->next_result());
+
+  $con->close();
+
+}
