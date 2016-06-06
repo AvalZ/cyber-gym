162c4c3786614fa13a9fe2fe7b85d2061f0a5224
avalz <avalenza89@gmail.com>
2016-06-06 18:16:16 +0200

    Graphic refactoring + explicit link to Capture Data page

--- public/support/capture_page.php
+++ public/support/capture_page.php
@@ -15,9 +15,39 @@ if (!empty(array_filter($_GET))) {
     $stmt->execute();
   }
 } else {
-  echo '<html>
-    <head><link rel="stylesheet" href="../css/bootstrap.min.css"></head><body><div class="container">
-<h1 class="text-center">Captured Data</h1><br>';
+  echo ' 
+    <html>
+    <head>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <style>body { padding-top: 70px; }</style>
+    </head>
+    <body>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="navbar-header">
+              <a href="/" class="navbar-brand">WebSec Tutorial</a>
+            </div>
+            <div class="collapse navbar-collapse">
+              <ul class="nav navbar-nav">
+                <li><a href="/">Home</a></li>
+                <li class="active"><a href="/support/capture_page.php">Capture</a></li>
+                <li><a href="/db">DB Setup</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+    <br>
+    <div class="panel panel-default">
+      <div class="panel-heading"><h2 class="text-center">Captured Data</h2></div>
+      <div class="panel-body">You can capture more data by issuing a GET request to this page ('. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] .'),
+      passing any data you want in the query string <br><i>e.g. ' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] . '?key=value</i></div>
+';
 
   $query = "SELECT * FROM datacapture ORDER BY timestamp DESC";
   $result = $con->query($query);
@@ -42,7 +72,7 @@ if (!empty(array_filter($_GET))) {
 
   echo "</table>";
 
-  echo "</div></html>";
+  echo "</div></div></html>";
 }
 
 $con->close();
