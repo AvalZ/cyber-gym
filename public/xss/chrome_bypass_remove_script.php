f6a46f331a179ccf2396713414442621009657a5
avalz <avalenza89@gmail.com>
2016-09-29 11:08:10 +0200

    Add Chrome Bypass by removing 'script'

---
+++ public/xss/chrome_bypass_remove_script.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!isset($_GET['first_name']) && !isset($_GET['last_name'])) {
+  echo "<h1>Hi! What's your name?</h1>";
+  echo "<form action='' method='GET'>";
+  echo "<input type='text' name='name'>";
+  echo "</form>";
+
+} else {
+  $first_name = preg_replace("script", "", $_GET['first_name'];
+  $last_name = preg_replace("script", "", $_GET['last_name'];
+  echo "<h1>Hi $first_name $last_name!</h1>";
+  //   "<h1>Hi <strong>Boh</strong>!</h1>"
+}
